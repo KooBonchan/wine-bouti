6f87249ad2fdc559e98c56665f40ad95d5e75a7b
KooBonchan <garam9koo@gmail.com>
2025-02-07 16:39:52 +0900

    cart modifier

--- src/main/java/com/winebouti/vo/CartDTO.java
+++ src/main/java/com/winebouti/vo/CartDTO.java
@@ -1,5 +1,6 @@
 package com.winebouti.vo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,12 +14,24 @@ import lombok.Setter;
 /*
  * Since cart will have no DB access, used term DTO instead of VO. 
  */
-@Getter @Setter
+@Getter
 public class CartDTO {
+  @Setter
 	private long memberId;
+  @Setter
   private Map<ProductVO, Integer> cartItems;
-  private int totalPrice;
   
+  private int totalPrice;
+  public void setTotalPrice() {
+    int total = cartItems.entrySet().stream()
+        .map(e-> e.getKey().getOriginalPrice() * e.getValue())
+        .reduce((a,b) -> a + b)
+        .orElse(-1);
+    if(total < 0) throw new ArithmeticException("Error occurred while calculating the total amount");
+    this.totalPrice = total; 
+  }
+  
+
   public PurchaseVO order(MemberVO memberVO) {
     String orderName = "";
     
@@ -34,17 +47,19 @@ public class CartDTO {
         .address(memberVO.getAddress())
         .products(products)
         .orderName(createOrderName())
+        .totalPrice(totalPrice)
         .build();
     return purchaseVO;
   }
+  
   
   private String createOrderName() {
     String baseName = "";
     for(ProductVO product : cartItems.keySet()) {
       baseName = product.getKoreanName();
+      break;
     }
     if(cartItems.size() <= 1) return baseName;
     return baseName + " 외 " + (cartItems.size() - 1) + "개";
-      
   }
 }
